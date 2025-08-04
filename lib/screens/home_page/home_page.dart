cfba0247f3b7ee0b8af4451b7e5e7eb45a9a3953
Miguel-Galli <miguelguedes.oli@gmail.com>
2025-08-03 21:16:31 -0300

    Cria botão filter, search e Tab Bar

--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -1,12 +1,42 @@
+import 'package:ecommerce_app/app_colors/app_colors.dart';
 import 'package:ecommerce_app/styles/home_screen_styles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   AppBar buildAppBar() {
     return AppBar(
+      bottom: TabBar(
+        tabAlignment: TabAlignment.center,
+        labelPadding: EdgeInsets.symmetric(horizontal: 22),
+        indicator: BoxDecoration(
+          color: Colors.transparent,
+        ),
+        isScrollable: true,
+        indicatorSize: TabBarIndicatorSize.label,
+        labelStyle: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        ),
+        unselectedLabelColor: AppColors.baseBlackColor,
+        labelColor: AppColors.baseGreenMilitaryColor,
+        tabs: [
+          Tab(
+            text: "All",
+          ),
+          Tab(
+            text: "Clothing",
+          ),
+          Tab(
+            text: "Shoes",
+          ),
+          Tab(
+            text: "Accessories",
+          ),
+        ],
+      ),
       backgroundColor: Colors.transparent,
       elevation: 0.0,
       centerTitle: true,
@@ -23,17 +53,40 @@ class HomePage extends StatelessWidget {
         ],
       ),
       actions: [
-        IconButton(onPressed: () {}, icon: SvgPicture.asset(assetName))
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.filter_alt_outlined,
+            color: AppColors.baseBlackColor,
+            size: 30,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.search,
+            color: AppColors.baseBlackColor,
+            size: 30,
+          ),
+        ),
       ],
     );
   }
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(),
-      body: Column(
-        children: [],
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: buildAppBar(),
+        body: TabBarView(
+          children: [
+            Center(child: Text("1Page")),
+            Center(child: Text("2Page")),
+            Center(child: Text("3Page")),
+            Center(child: Text("4Page")),
+          ],
+        ),
       ),
     );
   }
